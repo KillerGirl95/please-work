e4505c7017fd00ecf17dd1704c057a65dc19f4df
Norman <normanglad26012004@gmail.com>
2024-11-19 19:37:22 -0500

    fynal

--- controller/Login.js
+++ controller/Login.js
@@ -11,11 +11,11 @@ router.post('/login', async (req, res) => {
     try {
         // find user 
         const user = await Schema.findOne({ name });
-        if (!user) return res.status(401).json({ message: `username not found: ${name}` }); //find user 
+        if (!user) return res.status(401).json({ message: `username not found` }); //find user 
 
         // compare password 
         const isMatch = await user.comparePassword(password);
-        if (!isMatch) { return res.status(401).json({ message: 'Invalid password mf',user:user }); } //compare entered pwd with hashed pwd 
+        if (!isMatch) { return res.status(401).json({ message: 'Invalid password' }); } //compare entered pwd with hashed pwd 
 
         // generate jwt token 
         const token = jwt.sign({ id: user._id, username: user.username }, SECRET_KEY, { expiresIn: '1h' }); //sign jwt
