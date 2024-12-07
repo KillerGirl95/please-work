9d8c3e2759e31b63ffb2b00100f0ba48a83c7af9
Norman <normanglad26012004@gmail.com>
2024-12-07 15:10:23 -0500

    modd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BooksContextProvider } from './context/BookContext'
+import { AuthContextProvider } from './context/AuthContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <BooksContextProvider>
+        <App />
+      </BooksContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
